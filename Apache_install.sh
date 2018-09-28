a51691737f32956fb18bb2dacd41ffb9723b4b28
Dara <43659748+DaraUng@users.noreply.github.com>
2018-09-27 20:35:48 -0700

    Update Apache_install.sh
    add page to html

--- Apache_install.sh
+++ Apache_install.sh
@@ -3,3 +3,5 @@ systemctl enable httpd
 systemctl start httpd
 yum -y install mod_ssl
 systemctl restart httpd
+sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf
+echo '<html><body><h1>hit there </h1><body></html>' > /var/www/html/index.html
