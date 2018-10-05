69ecbe01db2b824ead01b59591d928473157a45a
Dara <43659748+DaraUng@users.noreply.github.com>
2018-10-04 20:26:10 -0700

    Update NTI-300-creating update_webpage.sh

--- NTI-300-creating update_webpage.sh
+++ NTI-300-creating update_webpage.sh
@@ -2,4 +2,4 @@
 
 
 time=$(date)
-echo "<html><body><h1>hit there $time, how are you? </h1><body></html>" > /var/www/html/index.html
+echo "<html><body><h1>hi there $time, how are you? </h1><body></html>" > /var/www/html/index.html
