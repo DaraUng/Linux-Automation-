33087e43247ae4ac271121778a6c81306705259b
Dara <43659748+DaraUng@users.noreply.github.com>
2018-12-06 19:51:59 -0800

    Update python_django.py
    removed some stuff

--- Django/python_django.py
+++ Django/python_django.py
@@ -1,4 +1,4 @@
-#! usr/bin/python
+#!usr/bin/python
     
 import os
     
@@ -13,12 +13,11 @@ os.chdir('/opt/django/')
 ##running python
 os.system('yum -y install python36')
 os.system('virtualenv -p python36 django')  
-os.system('source /opt/django/django/bin/activate && pip install django')
-os.chdir('/opt/django')
-os.system('source /opt/django/django/bin/activate ' + \
+
+os.system('source /opt/django/django/bin/activate && pip install django' + \
           '&& django-admin --version ' + \
           '&& django-admin startproject project1')
    
 os.system('chown -R daraung65 /opt/django')
-os.system('chown -R daraung65 /opt/django/project1')
+#os.system('chown -R daraung65 /opt/django/project1')
 os.system('sudo -u daraung65  sh -c "source /opt/django/django/bin/activate && python /opt/django/project1/manage.py runserver 0.0.0.0:8000&" ')
